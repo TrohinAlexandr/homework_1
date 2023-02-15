4be63480a5908cd3c8d62c49528b2929b6d1e751
Alex <rraxrawxx@mail.ru>
2023-02-15 16:44:44 +0300

    my homework

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+        task4();
+    }
+
+    public static void task4() {
+        System.out.println("Введите на какое кол-во месяцев берется вклад:");
+        int month = Integer.parseInt(scan());
+        if (month < 1) {
+            System.out.println("Ошибка! Кол-во месяцев должно быть больше нуля.");
+            return;
+        }
+        System.out.println("Введите месячный процент:");
+        double percent = Double.parseDouble(scan());
+        if (percent <= 0 || percent > 100) {
+            System.out.println("Ошибка! Процент должен быть больше нуля и не больше 100.");
+            return;
+        }
+        System.out.println("Введите начальную сумму:");
+        double sum = Double.parseDouble(scan());
+        if (sum <= 0) {
+            System.out.println("Ошибка! Первоначальная сумма вклада должна быть больше нуля.");
+            return;
+        }
+        int n = 1;
+        while (n <= month) {
+            sum += sum * (percent / 100);
+            System.out.println("Доход после " + n + "-го месяца составляет " + sum + " рублей.");
+            n++;
+        }
+        System.out.println("Итоговый доход за " + month + " месяцев составляет " + sum + " рублей.");
+    }
+
+    public static String scan() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+}
