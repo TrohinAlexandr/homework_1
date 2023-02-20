bb94d10b9ed7c0ec396def5bd067ef75c9dae395
TrohinAlexandr <90346619+TrohinAlexandr@users.noreply.github.com>
2023-02-20 13:36:19 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args) {
+        System.out.println("Трохин Александр Андреевич");
+        System.out.println("Группа РИБО-01-21, вариант 4"); 
         task4();
     }
 
